d5dfc0ada2ddefb21891ff7eade0c17d3f98f327
João Paulo da Silva <jhon001.jp@gmail.com>
2025-08-26 16:07:20 -0300

    Primeiro Commit - Finalização do Projeto

--- challenge-amigo-secreto_pt-main/app.js
+++ challenge-amigo-secreto_pt-main/app.js
@@ -1,1 +1,36 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+const inputAmigo =  document.querySelector("#amigo");
+const listaAmigos = document.querySelector("#listaAmigos");
+const resultado = document.querySelector("#resultado");
+
+
+let amigos = [];
+
+function adicionarAmigo() {
+    const nome = inputAmigo.value.trim(); // pega e limpa espaços
+    if (nome === "") {
+        alert("Você não digitou nada!");
+    } // não adiciona vazio
+
+    amigos.push(nome); // adiciona na lista
+
+    // cria um li para mostrar na página
+    const li = document.createElement("li");
+    li.textContent = nome;
+    listaAmigos.appendChild(li);
+
+    inputAmigo.value = ""; // limpa o input
+}
+function sortearAmigo() {
+    if (amigos.length === 0){
+        //lista vazia
+        alert("Adicione pelo menos um amigo!");
+        return;
+    }
+    // aqui continua o sorteio
+    const indice = Math.floor(Math.random()) * amigos.length;
+    const sorteado = amigos[indice];
+    resultado.textContent = `O amigo sorteado é: ${sorteado}`;
+    // remover o amigo da lista para não ser sorteado de novo
+    amigos.splice(indice, 1);
+}
